8e23e384f305907a3313fdf6caaac7b37720b976
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2021-09-22 12:28:39 +0200

    Added DerivedElliptoid

---
+++ Polymorphism/AbstractBaseClass/DerivedElliptoid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bif3.Swe1.Oop.Polymorphism.AbstractBaseClass
+{
+    class DerivedElliptoid : AbstractShape
+    {
+        public DerivedElliptoid(int x, int y) : base(x, y)
+        {
+        }
+
+        public override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PrintShapeType()
+        {
+            Console.WriteLine("I'm a elliptoid");
+        }
+    }
+}
