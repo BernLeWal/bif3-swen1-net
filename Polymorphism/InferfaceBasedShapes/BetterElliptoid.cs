64633d4a89c62d10496cdd83409f4b9f0bc6c67f
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2021-09-22 12:23:13 +0200

    added BetterElliptoid

---
+++ Polymorphism/InferfaceBasedShapes/BetterElliptoid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bif3.Swe1.Oop.Polymorphism.InferfaceBasedShapes
+{
+    class BetterElliptoid : IBetterShape
+    {
+        public int XOriginCoordinate => throw new NotImplementedException();
+
+        public int YOriginCoordinate => throw new NotImplementedException();
+
+        public void PrintShapeType()
+        {
+            Console.WriteLine("I'm an elliptoid!");
+        }
+
+        public void ShowOrigin()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
